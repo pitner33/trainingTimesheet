6224b2513349406fe76e4f5c564ea016fe60470a
Peter Kovacs <peter.kovacs@dpc.hu>
2020-01-14 13:43:29 +0100

    initial commit

---
+++ src/main/java/sol/dummies/trainingtimesheet/TrainingtimesheetApplication.java
@@ -0,0 +1,13 @@
+package sol.dummies.trainingtimesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingtimesheetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingtimesheetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sol/dummies/trainingtimesheet/TrainingtimesheetApplicationTests.java
@@ -0,0 +1,13 @@
+package sol.dummies.trainingtimesheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingtimesheetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
